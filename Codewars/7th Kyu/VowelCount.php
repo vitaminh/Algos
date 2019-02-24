30df32225a1a2ab9b42ad7fc6e3e2e933a60c0f2
Henry Do <661034+vitaminh@users.noreply.github.com>
2019-02-23 17:17:33 -0800

    Add PHP tags to enable code highlighting

--- Codewars/7th Kyu/VowelCount.php
+++ Codewars/7th Kyu/VowelCount.php
@@ -1,14 +1,16 @@
 // https://www.codewars.com/kata/54ff3102c1bad923760001f3
 
 // Return the number of vowels that occur within a given string
-function getCount($str) {
-  $vowelsCount = 0;
-  
-  $letters = str_split($str);
-  foreach ($letters as $letter) {
-    if ($letter == "a" || $letter == "e" || $letter == "i" || $letter == "o" || $letter == "u") {
-      $vowelsCount++;
+<?php
+  function getCount($str) {
+    $vowelsCount = 0;
+    
+    $letters = str_split($str);
+    foreach ($letters as $letter) {
+      if ($letter == "a" || $letter == "e" || $letter == "i" || $letter == "o" || $letter == "u") {
+        $vowelsCount++;
+      }
     }
+    return $vowelsCount;
   }
-  return $vowelsCount;
-}
+?>
--- Codewars/8th Kyu/FakeBinary.php
+++ Codewars/8th Kyu/FakeBinary.php
@@ -1,18 +1,19 @@
+
 /*
 Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
 */
-
-function fake_bin(string $s): string {
-  $result = "";
-  $stringArr = str_split($s);
-  foreach ($stringArr as $digit) {
-    if (intval($digit) >= 5) {
-      $result = $result . "1";
+<?php
+  function fake_bin(string $s): string {
+    $result = "";
+    $stringArr = str_split($s);
+    foreach ($stringArr as $digit) {
+      if (intval($digit) >= 5) {
+        $result = $result . "1";
+      }
+      else {
+        $result = $result . "0";
+      }
     }
-    else {
-      $result = $result . "0";
-    }
+    return $result;
   }
-  return $result;
-}
-
+?>
