33febdbe11540582e6328a711274faf4a5888a15
Henry Do <henry.do@gmail.com>
2020-02-25 13:29:42 -0800

    Codewars: 8th Kyu - Return Negative - C#

---
+++ Codewars/8th Kyu/ReturnNegative.cs
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/55685cd7ad70877c23000102/train/csharp
+
+using System;
+
+public static class Kata
+{
+    public static int MakeNegative(int number)
+    {
+        return number > 0 ? -(number) : number;
+    }
+}
