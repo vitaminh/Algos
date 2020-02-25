c482c2ff2bfde7a6a60d55f6e99e4f30c3a8d466
Henry Do <henry.do@gmail.com>
2020-02-24 18:05:51 -0800

    Codewars: 8th Kyu - Keep Hydrated - C#

---
+++ Codewars/8th Kyu/KeepHydrated.cs
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/582cb0224e56e068d800003c/solutions/csharp
+
+using System;
+
+public class Kata
+{
+    public static int Litres(double time)
+    {
+        return (int)Math.Floor(time * 0.5);
+    }
+}
